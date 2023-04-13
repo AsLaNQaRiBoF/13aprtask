85064260ad87b81ce695bf40066eeb19463878f1
aslan <aslanqaribov7@gmail.com>
2023-04-13 14:09:14 +0400

    first commit

---
+++ ConsoleApp1/Exam.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Exam
+    {
+        public string Studentname
+        {
+            get { return Studentname; }
+            set
+            {
+                if (value.Length > 3)
+                    Studentname=value;
+            }
+        }
+        public string Subject
+        {
+            get { return Subject; }
+            set
+            {
+                if (value.Length > 3)
+                    Subject=value;
+            }
+        }
+        public int Point
+        {
+            get { return Point; }
+            set
+            {
+                if (value >= 0 && value<=100) 
+                    Point=value;
+            }
+        }
+        public DateTime Startdata 
+        {
+            get;
+            set;
+           
+        }
+        public DateTime Enddata
+        {
+            get;
+            set;
+        }
+        public int Duration
+        {
+            get; set;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam>exams = new List<Exam>();
+            bool check = false;
+
+            do
+            {
+                Console.WriteLine("student nameni yaz");
+                string Stdname = Console.ReadLine();
+                Console.WriteLine("student point yaz")
+                int point = int.Parse(Console.ReadLine());
+                Console.WriteLine("subjecti yaz");
+                string Subject = Console.ReadLine();
+                Console.WriteLine("starttime yaz");
+                DateTime startdate =DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("endtime yaz");
+                DateTime enddate =DateTime.Parse(Console.ReadLine());
+                Exam exam = new Exam()
+                {
+                    Studentname=Stdname,
+                    Point=point,
+                    Subject=Subject,
+                    Startdata=startdate,
+                    Enddata=enddate,
+
+                };
+
+                exams.Add(exam);
+                var Point = exams.FindAll(x => x.Point>50);
+                Point.ForEach(x =>
+                {
+                    Console.WriteLine(x.Point, x.Studentname, x.Subject, x.Duration);
+                });
+                var longDurationexams = exams.FindAll(x => x.Duration>TimeSpan.FromHours(1));
+
+            }while(check=true);
+            longDurationexams.Foreach(x =>
+            {
+
+                Console.WriteLine(x.Point, x.Studentname, x.Subject, x.Duration);
+            });
+
+            }
+        }
+    }
+}
